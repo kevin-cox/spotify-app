c923aa4cea900b1c6733c5885db6be589b2592f2
kevin-cox <kjc3hc@virginia.edu>
2020-11-12 11:26:50 -0500

    asdf

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <a href='http://localhost:8888'>
+        <button>Login with Spotify</button>
+      </a>
+    </div>
+  );
+}
+
+export default App;
